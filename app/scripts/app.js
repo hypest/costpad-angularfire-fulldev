a42b99dd1169b89026935f5049959a870aadd915
hypest <stefanostogoulidis@gmail.com>
2014-09-13 23:42:37 +0000

    Initial code commit, using generator-angularfire

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name costpadApp
+ * @description
+ * # costpadApp
+ *
+ * Main module of the application.
+ */
+angular.module('costpadApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.utils',
+    'simpleLogin'
+  ]);
